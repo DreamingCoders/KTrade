d4a7afb7211f496e3debbe67d138b6f8fccbd085
Maui <61954544+DreamingCoders@users.noreply.github.com>
2025-09-14 03:10:55 -0400

    Create main.go
    Added trade template.

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/session"
+)
+
+// Coming soon got the template done tho.
